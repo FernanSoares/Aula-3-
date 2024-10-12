80e3d4e42a407ea78fe1e527aa4100482237777a
Fernando Soares <130779847+FernanSoares@users.noreply.github.com>
2024-10-11 21:15:26 -0300

    Add files via upload

---
+++ Aula03/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
+import { PrincipalComponent } from './principal/principal.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    PrincipalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
